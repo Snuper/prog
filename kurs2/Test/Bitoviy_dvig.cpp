32129433f1c61bced02552a8283697c08c86815d
Snuper <47518901+Snuper@users.noreply.github.com>
2020-03-05 13:01:04 +0700

    перешел на bitset

--- kurs2/Test/Bitoviy_dvig.cpp
+++ kurs2/Test/Bitoviy_dvig.cpp
@@ -3,83 +3,81 @@
 #include <stdlib.h>
 #include <cstdio>
 #include <time.h>
+#include <bitset>
 
 using namespace std;
 
-const unsigned char flag_overflow = 0x01;
-const unsigned char flag_0 = 0x02;
-const unsigned char flag_memoryBorder = 0x08;
-const unsigned char flag_tactIgnore = 0x10;
-const unsigned char flag_wrongCommand = 0x20;
+const int flag_overflow = 0;
+const int flag_0 = 1;
+const int flag_memoryBorder = 2;
+const int flag_tactIgnore = 3;
+const int flag_wrongCommand = 4;
 
-unsigned char flags = 0;
+bitset<5> bits(0x2);
 
-int flag_activate(int temp)
+void flag_set(int temp)
 {
-	if (temp == 1)
-	{
-		if (flags & flag_overflow) flags &= ~flag_overflow;
-		else flags |= flag_overflow;
-		return 0;
-	} 
-	else if (temp == 2)
-	{
-		if (flags & flag_0) flags &= ~flag_0;
-		else flags |= flag_0;
-		return 0;
-	}
-	else if (temp == 3)
-	{
-		if (flags & flag_memoryBorder) flags &= ~flag_memoryBorder;
-		else flags |= flag_memoryBorder;
-		return 0;
-	}
-	else if (temp == 4)
-	{
-		if (flags & flag_tactIgnore) flags &= ~flag_tactIgnore;
-		else flags |= flag_tactIgnore;
-		return 0;
-	}
-	else if (temp == 5)
-	{
-		if (flags & flag_wrongCommand) flags &= ~flag_wrongCommand;
-		else flags |= flag_wrongCommand;
-		return 0;
-	}
-	return 1;
+	if (temp == 1) bits.set(flag_overflow);
+	else if (temp == 2) bits.set(flag_0);
+	else if (temp == 3) bits.set(flag_memoryBorder);
+	else if (temp == 4) bits.set(flag_tactIgnore);
+	else if (temp == 5) bits.set(flag_wrongCommand);
 }
 
-void flag_sostoyanie()
+void flag_reset(int temp)
 {
-	if (flags & flag_overflow) cout << "flag_overflow - True" << endl;
-	else cout << "flag_overflow - False" << endl;
-	
-	if (flags & flag_0) cout << "flag_0 - True" << endl;
-	else cout << "flag_0 - False" << endl;
-	
-	if (flags & flag_memoryBorder) cout << "flag_memoryBorder - True" << endl;
-	else cout << "flag_memoryBorder - False" << endl;
-	
-	if (flags & flag_tactIgnore) cout << "flag_tactIgnore - True" << endl;
-	else cout << "flag_tactIgnore - False" << endl;
-	
-	if (flags & flag_wrongCommand) cout << "flag_wrongCommand - True" << endl;
-	else cout << "flag_wrongCommand - False" << endl;
+	if (temp == 1) bits.reset(flag_overflow);
+	else if (temp == 2) bits.reset(flag_0);
+	else if (temp == 3) bits.reset(flag_memoryBorder);
+	else if (temp == 4) bits.reset(flag_tactIgnore);
+	else if (temp == 5) bits.reset(flag_wrongCommand);
+}
+
+void flag_flip(int temp)
+{
+	if (temp == 1) bits.flip(flag_overflow);
+	else if (temp == 2) bits.flip(flag_0);
+	else if (temp == 3) bits.flip(flag_memoryBorder);
+	else if (temp == 4) bits.flip(flag_tactIgnore);
+	else if (temp == 5) bits.flip(flag_wrongCommand);
+}
+
+void flag_test(int temp)
+{
+	if (temp == 0)
+	{
+		cout << endl << "flag_overflow = " << bits.test(flag_overflow) << endl;
+		cout << endl << "flag_0 = " << bits.test(flag_0) << endl;
+		cout << endl << "flag_memoryBorder = " << bits.test(flag_memoryBorder) << endl;
+		cout << endl << "flag_tactIgnore = " << bits.test(flag_tactIgnore) << endl;
+		cout << endl << "flag_wrongCommand = " << bits.test(flag_wrongCommand) << endl;
+	}
+	else if (temp == 1) cout << endl << "flag_overflow = " << bits.test(flag_overflow) << endl;
+	else if (temp == 2) cout << endl << "flag_0 = " << bits.test(flag_0) << endl;
+	else if (temp == 3) cout << endl << "flag_memoryBorder = " << bits.test(flag_memoryBorder) << endl;
+	else if (temp == 4) cout << endl << "flag_tactIgnore = " << bits.test(flag_tactIgnore) << endl;
+	else if (temp == 5) cout << endl << "flag_wrongCommand = " << bits.test(flag_wrongCommand) << endl;
 }
 
 int main(void)
 {
-	int temp_flag = 1;
-	while (temp_flag != 0)
+	flag_reset(2);
+	for (int temp_flag = -1, temp_act; temp_flag != 0;)
 	{
 		cout << "Vibor flaga: ";
 		cin >> temp_flag;
 		if (temp_flag < 0 or temp_flag > 5) cout << "Erorr" << endl;
 		else
 		{
-			flag_activate(temp_flag);
-			flag_sostoyanie();
+			cout << "Viberite deistvie:" << endl << "1 - vkluchaet vibrany flag" << endl << "2 - Menyaet znachenie flaga na protivopolojnoe" << endl << "3 - Vikluchaet flag" << endl << "4 - uznat znachenie flaga" << endl;
+			cin >> temp_act;
+			if (temp_act < 1 or temp_act > 4) cout << "Erorr" << endl;
+			else if (temp_act == 1) flag_set(temp_flag);
+			else if (temp_act == 2) flag_flip(temp_flag);
+			else if (temp_act == 3) flag_reset(temp_flag);
+			else if (temp_act == 4) flag_test(temp_flag);
 		}
+		flag_test(0);
 		system("pause");
 		system("cls");
 	}
