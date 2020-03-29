b17f28c314a7975e7bf7d77b124438cab0c9a1f1
Snuper <47518901+Snuper@users.noreply.github.com>
2020-03-29 14:33:00 +0700

    Основа готова

--- kurs2/VIochMath/laby_rubanchiku/lab5/lagrnhz.cpp
+++ kurs2/VIochMath/laby_rubanchiku/lab5/lagrnhz.cpp
@@ -5,23 +5,29 @@ using namespace sf;
 
 double formula(double x)
 {
-	return x * x;
+	return sqrt(x);
 }
 
 int main()
 {	
-	RenderWindow window(VideoMode(720, 720), "lagranhzz");
-	double shag = 1, x = 0;
+	View view;
+	view.reset(FloatRect(300, 300, 120, 120));
+	
+	ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	
+	RenderWindow window(VideoMode(720, 720), "lagranhzz", Style::Default, settings);
+	double shag = 1, x = 0, kolvo = 720 / shag;
 	
 	VertexArray os_x(LineStrip, 2);
 	VertexArray os_y(LineStrip, 2);
-//	VertexArray lines(LineStrip, 720);
+	VertexArray lines(LineStrip, kolvo);
 
-	os_x[0].position = Vector2f(360, 360);
-	os_x[1].position = Vector2f(-360, 360);
+	os_x[0].position = Vector2f(720, 360);
+	os_x[1].position = Vector2f(0, 360);
 	
-	os_y[0].position = Vector2f(360, 360);
-	os_y[1].position = Vector2f(360, -360);
+	os_y[0].position = Vector2f(360, 720);
+	os_y[1].position = Vector2f(360, 0);
 	
 	while (window.isOpen())
 	{
@@ -32,15 +38,23 @@ int main()
 				window.close();
 		}
 		
-//		for(int i = 0, x = -360; i < 720; i++, x += shag)
-//		{
-//			lines[i].position = Vector2f(360 + x, 360 - formula(x));
-//		}
- 		
+		for(int i = 0, x = -360; i < kolvo; i++, x += shag)
+		{
+			lines[i].position = Vector2f(360 + x, 360 - formula(x));
+		}
+		
+		if (Keyboard::isKeyPressed(Keyboard::D)) view.move(0.01, 0);
+		else if (Keyboard::isKeyPressed(Keyboard::S)) view.move(0, 0.01);
+		else if (Keyboard::isKeyPressed(Keyboard::A)) view.move(-0.01, 0);
+		else if (Keyboard::isKeyPressed(Keyboard::W)) view.move(0, -0.01);
+		else if (Keyboard::isKeyPressed(Keyboard::E)) view.zoom(1.001f);
+		else if (Keyboard::isKeyPressed(Keyboard::Q)) view.zoom(0.999f);
+		 		
 		window.clear();
+		window.setView(view);
 	 	window.draw(os_x);
 	 	window.draw(os_y);
-//		window.draw(lines);
+		window.draw(lines);
 		window.display();
 	}
 	
