e5f3e82bc9f62406feb32df4b35970e9e09f5f2e
Snuper <47518901+Snuper@users.noreply.github.com>
2020-04-13 19:30:56 +0700

    визуальный мусор
    да да да, добавь всё в гитигнор идиот, но мне лень, как нибудь потом

--- kurs2/VisualProgram/lab4()no[e/Проект lab_4/Form1.cs
+++ kurs2/VisualProgram/lab4()no[e/Проект lab_4/Form1.cs
@@ -102,7 +102,7 @@ namespace VP_4
 
         private void обАвтореToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Филимонов Владислав\nПриходченко Иван\nИП-814");
+            MessageBox.Show("Якунин Андрей\nПарфенцов Семен\nИП-814");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
