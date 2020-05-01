95eb6c0f8be52968a96dcdd38871e08e007fd736
Snuper <47518901+Snuper@users.noreply.github.com>
2020-05-01 13:04:30 +0700

    сплайны, почти рабочая верси
    сохраню ее, чтоб откатиться
    
    теперь можно ломать ее)

--- kurs2/VIochMath/laby_rubanchiku/lab8/spline.cpp
+++ kurs2/VIochMath/laby_rubanchiku/lab8/spline.cpp
@@ -5,12 +5,141 @@ using namespace sf;
 
 double formula(double x)
 {
-	return sqrt(x);
+	return sin(x);
+}
+
+void c_matrix(double **c_mas, double *h_mas, int n)
+{
+	cout << endl << endl;
+	for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) c_mas[i][j] = 0;
+	for(int i = 0, temp = 3; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(i == j)
+			{
+				c_mas[i][j] = (h_mas[i] + h_mas[i + 1]) / 3;
+				c_mas[i][j + 1] = h_mas[temp] / 6;
+				c_mas[i + 1][j] = h_mas[i + 2] / 6;
+			}
+			cout << "	" << c_mas[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void d_vector(double *y_mas, double *h_mas, double *d_mas, int n)
+{
+	cout << endl;
+	for(int i = 1; i < n + 1; i++)
+	{
+		d_mas[i - 1] = ((y_mas[i + 1] - y_mas[i]) / h_mas[i]) - ((y_mas[i] - y_mas[i - 1]) / h_mas[i - 1]);
+		cout << endl << "d[" << i - 1 << "] = " << d_mas[i - 1];
+	}
+}
+
+void gaus(double **c_mas, double *d_mas, int size, double *m_mas)
+{
+	double **A = new double* [size];
+	for(int i = 0; i < size; i++) A[i] = new double [size];
+	
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0; j < size; j++) A[i][j] = c_mas[i][j];
+		 A[i][size] = d_mas[i];
+	}
+	
+	float mnozhitel;
+	int i, j, dioganal, temp;
+	
+	cout << endl << endl << "------Matrix - A------" << endl;
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0; j < size + 1; j++)
+		{
+			cout << " " << round(A[i][j]*100)/100 << " ";
+		}
+		cout << endl;
+	}
+	cout << "------Matrix - A------" << endl;
+	
+	for(dioganal = 0; dioganal < size-1; dioganal++)
+	{
+		cout << endl << " dioganal = " << dioganal << endl;
+		for(i = 1 + dioganal; i < size; i++)
+		{
+			j = 0;
+			mnozhitel = A[i][dioganal] / A[dioganal][dioganal];
+			cout << "  mnozhitel= " << mnozhitel << "   A[dioganal][dioganal] = " << A[dioganal][dioganal] << "   A[i][dioganal]=" << A[i][dioganal] << endl;
+			for(j = 0; j < size + 1; j++)
+			{
+				A[i][j] -= mnozhitel * A[dioganal][j];
+				if(A[i][j] == 0) if(A[dioganal][j] != 0) cout << "  A[" << i << "][" << j << "] = " << A[i][j] << endl;
+			}
+		}
+		cout << endl << "------Matrix - A------" << endl;
+		for(int i = 0; i < size; i++)
+		{
+			for(int j = 0; j < size + 1; j++)
+			{
+				cout << " " << round(A[i][j]*100)/100 << " ";
+			}
+			cout << endl;
+		}
+		cout << "------Matrix - A------" << endl;
+	}
+	
+	for(dioganal = 1, mnozhitel = 1; dioganal < size + 1; dioganal++)
+	{
+		for(temp = dioganal - 1; temp > 0; temp--) A[size - dioganal][size] -= A[size - dioganal][size - temp];
+		mnozhitel = A[size - dioganal][size] / A[size - dioganal][size - dioganal];
+		for(temp = size - 1; temp >= 0; temp--) A[temp][size - dioganal] *= mnozhitel;
+		cout << "x" << size - dioganal + 1 << " = " << mnozhitel << endl;
+		m_mas[size-dioganal + 1] = mnozhitel; 
+	}
+}
+
+double s_formula(double sp_x, double *x_mas, double *h_mas, double *y_mas, double *m_mas, int i)
+{
+	double sk1, sk2, sk3, sk4 ,sk5 ,sk6, temp_ch, temp_zn;
+	
+	cout << endl << "RTEETST" << endl;
+	
+	temp_ch = pow((x_mas[i] - sp_x), 3);
+	temp_zn = 6 * h_mas[i];
+	sk1 = m_mas[i - 1] * temp_ch / temp_zn;
+	cout << endl << sk1 << endl;
+	
+	temp_ch = pow((sp_x - x_mas[i - 1]), 3);
+	temp_zn = 6 * h_mas[i];
+	sk2 = m_mas[i] * temp_ch / temp_zn;
+	cout << endl << sk2 << endl;
+	
+	temp_ch = y_mas[i - 1] * m_mas[i - 1] * pow(h_mas[i], 2);
+	temp_zn = 6;
+	sk3 = temp_ch / temp_zn;
+	cout << endl << sk3 << endl;
+	
+	temp_ch = x_mas[i] - sp_x;
+	temp_zn = h_mas[i];
+	sk4 = temp_ch / temp_zn;
+	cout << endl << sk4 << endl;
+	
+	temp_ch = y_mas[i] * m_mas[i] * pow(h_mas[i], 2);
+	temp_zn = 6;
+	sk5 = temp_ch / temp_zn;
+	cout << endl << sk5 << endl;
+	
+	temp_ch = sp_x - x_mas[i - 1];
+	temp_zn = h_mas[i];
+	sk6 = temp_ch / temp_zn;
+	cout << endl << sk6 << endl;
+	
+	return (sk1 + sk2 + sk3 * sk4 + sk5 * sk6);
 }
 
 int main()
 {
-//	ќбъ€вл€ем
 	int i, n, temp_i, swap_1, swap_2;	
 	double shag = 0.01;
 	
@@ -26,17 +155,18 @@ int main()
 	VertexArray os_x(LineStrip, 2);
 	VertexArray os_y(LineStrip, 2);
 	VertexArray lines(LineStrip, kolvo);
-	
+	VertexArray lines_sp(LineStrip, kolvo);
 	VertexArray sp_g(LineStrip, 2);
-//	ќбъ€вл€ем
-//	»ницилизируем массивы и заполн€ем массивы
+	
 	cout << endl << "Vvedite kolvo tochek interpolyacii: ";
 	cin >> n;
 	
-	double y_mas[n], x_mas[n], h_mas[n - 1], eps = 10000, raznost[n][n];
+	double s, d_mas[n - 2], y_mas[n], x_mas[n], h_mas[n - 1], m_mas[n - 2], eps = 10000, raznost[n][n];
+	double **c_mas = new double* [n - 2];
+	for (i = 0; i < n - 1; i++) c_mas[i] = new double [n - 2];
 	
 	VertexArray x_g(LineStrip, n*3);
-	for(temp_i = 0; temp_i < n; temp_i++, i += 3)
+	for(temp_i = 0, i = 0; temp_i < n; temp_i++, i += 3)
 	{
 		cout << endl << "x[" << temp_i << "] = "; 
 		cin >> x_mas[temp_i];
@@ -65,8 +195,19 @@ int main()
 	
 	os_y[0].position = Vector2f(360, 720);
 	os_y[1].position = Vector2f(360, 0);
-//	»ницилизируем массивы и заполн€ем массивы
 	
+	cout << endl << "Vvedite x: ";
+	cin >> sp_x;
+	
+//	
+	c_matrix(c_mas, h_mas, n - 2);
+	d_vector(y_mas, h_mas, d_mas, n - 2);
+	gaus(c_mas, d_mas, n - 2, m_mas);
+	sp_y = s_formula(sp_x, x_mas, h_mas, y_mas, m_mas, 2);
+	sp_g[0].position = Vector2f(360 + sp_x, 360);
+	sp_g[1].position = Vector2f(360 + sp_x, 360 - sp_y);
+	sp_g[0].color = Color::Red;
+//	
 	while (window.isOpen())
 	{
 		Event event;
@@ -79,6 +220,8 @@ int main()
 		for(i = 0, x = -360 * shag; i < kolvo; i++, x += shag)
 		{
 			lines[i].position = Vector2f(360 + x, 360 - formula(x));
+			lines_sp[i].position = Vector2f(360 + x, 360 - s_formula(x, x_mas, h_mas, y_mas, m_mas, 2));
+			lines_sp[i].color = Color::Red;
 		}
 		
 		if (Keyboard::isKeyPressed(Keyboard::D)) view.move(0.001 + temp, 0);
@@ -103,6 +246,7 @@ int main()
 		window.draw(os_x);
 	 	window.draw(os_y);
 		window.draw(lines);
+		window.draw(lines_sp);
 		window.display();
 	}
 	
