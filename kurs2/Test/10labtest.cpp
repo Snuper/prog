079ace07ba54275b2b8db1066d2bcefd9c29fe8b
Snuper <47518901+Snuper@users.noreply.github.com>
2020-05-21 12:56:41 +0700

    численное интегрирование
    раш по лабам

--- kurs2/Test/10labtest.cpp
+++ kurs2/Test/10labtest.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
-//0.6931471805599453
+#define nullptr NULL
 
 using namespace std;
 
@@ -12,58 +12,40 @@ double formula(double x)
 	return 1 / x;
 }
 
-double ch_in(double h, int n, double *y_mas, int flag)
+double ch_in(double h, int n, double *y_mas)
 {
-	double otvet, per = y_mas[0] + y_mas[n - 1], vto = 0, tre = 0;
-	if(flag == 1)
-	{
-		otvet = h / 3;
-		
-		for(int i = 1; i < n - 1; i += 2) vto += y_mas[i];
-		for(int i = 2; i < n - 2; i += 2) tre += y_mas[i];
-	
-		vto *= 4;
-		tre *= 2;
-		
-		otvet *= per + vto + tre;
-	}
-	else
-	{
-		otvet = h;	
-		per *= 1.0/2.0;
-				
-		for(int i = 1; i < n - 1; i ++) vto += y_mas[i];
-		
-		otvet *= per + vto;
-	}
+	double otvet = h / 3, per = y_mas[0] + y_mas[n - 1], vto = 0, tre = 0;
 
-	return otvet;
+	for(int i = 1; i < n - 1; i += 2) vto += y_mas[i];
+	for(int i = 2; i < n - 2; i += 2) tre += y_mas[i];
+
+	vto *= 4;
+	tre *= 2;
+	
+	cout << endl << "OTEVT DO = " << otvet;
+		cout << endl << "per = " << per;
+		cout << endl << "vto = " << vto;
+		cout << endl << "tre = " << tre;
+	otvet *= per + vto + tre;
+	
+		
+		cout << endl << "OTEVT posle = " << otvet;
+	
+	//return  round(otvet * eps) / eps;
+	return  otvet;
 }
 
-void pogreshnost(double h, double b, double a, int flag)
+void pogreshnost(double h, double b, double a)
 {
-	double E_us, temp_h = h, M, E_okr = eps, E_real;
+	double E_us, temp_h = h, M, E_okr = 1 / eps, E_real;
 	
-	if(flag == 1)
-	{
-		for(int i = 0; i < 3; i++) h *= temp_h;
-		
-		M = 24;
-		
-		E_us = h / 180 * (b - a) * M;
-		
-		E_real = E_us + E_okr;
-	}
-	else
-	{
-		for(int i = 0; i < 1; i++) h *= temp_h;
-		
-		M = 2;
-		
-		E_us = h / 12 * (b - a) * M;
-		
-		E_real = E_us + E_okr;
-	}
+	for(int i = 0; i < 3; i++) h *= temp_h;
+	
+	M = 24;
+	
+	E_us = h / 180 * (b - a) * M;
+	
+	E_real = E_us + E_okr;
 	
 	cout << endl << "E_usechenaya: " << E_us;
 	cout << endl << "E_okruglenie: " << E_okr;
@@ -73,7 +55,7 @@ void pogreshnost(double h, double b, double a, int flag)
 main()
 {
 	int n, i;
-	double left, right, h, I1, I2;
+	double left, right, h, I1, I2, toch_otvet = 0.6931471805599453;
 	
 	cout << endl << "Vvedite granicy[Left][Right]: ";
 	cin >> left >> right;
@@ -97,32 +79,25 @@ main()
 		x_mas[i] = x_mas[i - 1] + h;
 		y_mas[i] = formula(x_mas[i]);
 	}
-	
-	int flag;
-	cout << endl << "Simson - 1\nTrapeciya - 2" << endl;
-	cin >> flag;
-	if(flag < 1 or flag > 2) return 1;
-	
-	I1 = ch_in(h, n, y_mas, flag);
+	cout << n << endl;
+	I1 = ch_in(h, n, y_mas);
 	cout << endl << "Otvet: " << I1;
 	
 	cout << endl << "Pogrshnost: " << endl;
-	pogreshnost(h, x_mas[n - 1], x_mas[0], flag);
+	//pogreshnost(h, x_mas[n - 1], x_mas[0]);
 	
 	cout << endl << "---------------Dvoinoi perechet-----------------" << endl;
 	
 	bool good = false;
-	int step = 0;
-
+	int o = 1;
 	while(good == false)
 	{
 		h /= 2;
-		
 		delete [] x_mas;
 		delete [] y_mas;
 		
 		n = (right / h) - (left / h) + 1;
-
+		cout << n << endl;
 		double *x_mas = nullptr;
 		x_mas = new double [n];
 		
@@ -137,23 +112,22 @@ main()
 			y_mas[i] = formula(x_mas[i]);
 		}
 		
-		I2 = ch_in(h, n, y_mas, flag);
-		
-		step++;
-		cout << endl << "Shag " << step;
+		I2 = ch_in(h, n, y_mas);
+			cout << endl << "Shag " << o;
 		cout << endl << "Korektirovke " << I1 - I2 << " < " << eps;
 		cout << endl << "I1 " << I1;
-		cout << endl << "I2 " << I2 << endl;
-		
+		cout << endl << "I2 " << I2;
+		//cout << endl << "Otvet: " << I2;
+			//cout << endl << "Tochny Otvet: " << round(toch_otvet * eps) / eps;
+		//cout << endl << "Tochny Otvet: " << toch_otvet;
 		if(fabs(I1 - I2) < eps)
 		{
-			cout << endl << "----------------------------------";
 			cout << endl << "Otvet: " << I2;
-			cout << endl << "Kolvo shagod: " << step;
+			//cout << endl << "Tochny Otvet: " << round(toch_otvet * eps) / eps;
 			good = true;
 		}
-		
-		I1 = ch_in(h, n, y_mas, flag);
+		I1 = ch_in(h, n, y_mas);
+		o += 1;
 	}
 	
 	return 0;
