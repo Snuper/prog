11d5e56885ee443761c2f39941cb308bf518663f
Snuper <47518901+Snuper@users.noreply.github.com>
2020-06-05 14:52:35 +0700

    rub kurs
    исправил ошибки
    1 - в алгоритме решения k4 (h/2)
    2 - сравнивал не те числа в довйном пересчете(условия для нахождения i*2 недостаточно )

--- kurs2/Test/okruglenie.cpp
+++ kurs2/Test/okruglenie.cpp
@@ -1,201 +1,212 @@
-#include <stdio.h>
 #include <iostream>
 #include <cmath>
-#include <string>
-#include <iomanip> 
+#include <iomanip>
+#include <locale.h>
 
 using namespace std;
 
-const double eps = 0.00001, e = 2.71828182846;
-
-int opredelitel()
+double f(double x, double y, double yProiz)
 {
-	return 2;
+	return (exp(x) + y + yProiz) / 3;
 }
 
-double formula(double x, double *y_mas)
+int main()
 {
-	return (pow(e, x) + y_mas[0] + y_mas[1]) / 3;
-//	return x * y_mas[1] + y_mas[0];
-// 	return x + y_mas[0] + x * y_mas[1] - y_mas[2];
-}
+	
+	setlocale(LC_ALL,"RUS");
+	
+		
+	double y[10000];
+	double x1[10000];
+	double x2[10000];
+	
+	double yProiz[10000];
+	
+	
+	double y051[10000];
+	double y052[10000];
 
-void du_4(double x_first, double x_last, double h, double *y_mas, int n, double *y_mas_for_chek)
-{
-//	cout << endl << "Zahli v funciu: -----------------------------------------" << endl;
-//	cout << endl << "x_first = " << x_first << endl;
-//	cout << endl << "x_last = " << x_last << endl;
-	
-	int du_step = 1;
-	double **k_mas;
-	k_mas = new double* [4];
-    for (int i = 0; i < 4; i++) k_mas[i] = new double [n];
-    
-    double *save_y_mas = nullptr;
-	save_y_mas = new double [n];
-//	cout << endl << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO EVROPA OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << endl;
-	for (int i = 0; i < n; i++)
-	{
-		save_y_mas[i] = y_mas[i];
-//		cout << endl << save_y_mas[i] << endl;
-		y_mas[i] = 0;
-	}
-//	cout << endl << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO EVROPA OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << endl;
-//	for(int i = 0; i < n; i++) cout << endl << "y_mas = " << y_mas[i] << endl;
-//	for(int i = 0; i < n; i++) cout << endl << "save_y_mas = " << save_y_mas[i] << endl;
-	
-	for(double x = x_first; x <= x_last; x += h, du_step++)
-	{
-//		cout << endl << "----------------------------------------|du_Step" << du_step << "|----------------------------------------" << endl;
-//		cout << endl << "x = " << x << endl;
-		
-//		cout << endl << "--------\\" << endl;
-		for(int k = 1; k < 5; k++)
-		{
-			if(k == 1)
-			{
-				for(int q = n - 1, z = 0; q > 0; q--, z++)
-				{
-					k_mas[0][z] = save_y_mas[q];
-//					cout << endl << "Neochen test y_mas = " << save_y_mas[q] << "  i konechno je q = " << q;
-//					cout << endl << "Neochen test k_mas = " << k_mas[0][z] << "  i konechno je z = " << z;
-				}
-				k_mas[0][n - 1] = formula(x, save_y_mas);
-//				for(int i = 0; i < n; i++) cout << endl << "	k1_mas = " << k_mas[0][i] << endl;
-			}
-			else if(k == 2)
-			{
-				for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[0][i];
-				for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[1][z] = y_mas[q];
-				k_mas[1][n - 1] = formula(x + h / 2, y_mas);
-				for (int i = 0; i < n; i++) y_mas[i] = 0;
-//				for(int i = 0; i < n; i++) cout << endl << "	k2_mas = " << k_mas[1][i] << endl;
-			}
-			else if(k == 3)
-			{
-				for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[1][i];
-				for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[2][z] = y_mas[q];;
-				k_mas[2][n - 1] = formula(x + h /2, y_mas);
-				for (int i = 0; i < n; i++) y_mas[i] = 0;
-//				for(int i = 0; i < n; i++) cout << endl << "	k3_mas = " << k_mas[2][i] << endl;
-			}
-			else if(k == 4)
-			{
-				for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[2][i];
-				for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[3][z] = y_mas[q];;
-				k_mas[3][n - 1] = formula(x + h, y_mas);
-				for (int i = 0; i < n; i++) y_mas[i] = 0;
-//				for(int i = 0; i < n; i++) cout << endl << "	k4_mas = " << k_mas[3][i] << endl;
-			}
-		}
-//		cout << endl << "--------/" << endl;
-		
-		for(int i = 0; i < n; i++)
-		{
-			y_mas[i] =  save_y_mas[i] + h / 6 * (k_mas[0][i] + 2 * k_mas[1][i] + 2 * k_mas[2][i] + k_mas[3][i]);
-			save_y_mas[i] = y_mas[i];
-		}
-		
-//		for(int i = 0; i < n; i++) cout << endl << "y_mas = " << y_mas[i] << endl;
-		
-//		cout << endl << "JJJJJJJJJJJJJJKKKKKKKKKJJJJJJJJJJJJJJJJJKKKKKKKKKKJJJJJJ" << endl;
-		y_mas_for_chek[du_step - 1] = y_mas[0];
-//		cout << endl << y_mas_for_chek[du_step - 1] << "    " << y_mas[0] << "      " << du_step - 1 << endl;
-//		cout << endl << "JJJJJJJJJJJJJJKKKKKKKKKJJJJJJJJJJJJJJJJJKKKKKKKKKKJJJJJJ" << endl;
-//		cout << endl;
-	}
-	
-	delete [] k_mas;
-}
+	y[0]=1;
+	yProiz[0]=1;
+	x1[0]=0;
+	x2[0]=0;
 
-main()
-{
-	double x_first, x_last, h, I1, I2;
 	
-	cout << endl << "1) x_first\n2) x_last\n3) Shag" << endl;
-	cin >> x_first >> x_last >> h;
+		int k=0;
+	double t=50;
+	double raz;
+	double h1=0.1;
+//	long double h1=0.2;
+	double h2=0.05;
+	double yOtv1[10000];
+	double yOtv2[10000];
+	double xOtv1[10000];
+	double yProizOtv1[10000];
+	double yProizOtv2[10000];
 	
-	int n, step = 0, n_shag = (x_last / h) - (x_first / h) + 1;
-	string temp_y = "y";
-	n = opredelitel();
-	
-	double *y_mas = nullptr;
-	y_mas = new double [n];
-	
-	double *temp_y_mas = nullptr;
-	temp_y_mas = new double [n];
-	
-	cout << endl << "Vvedite nachalnye usloviya: ";
-	for(int i = 0; i < n; i++)
-	{
-		cout << endl << temp_y << " = ";
-		cin >> y_mas[i];
-		temp_y_mas[i] = y_mas[i];
-		temp_y += "'";
-	}
-	
-	bool good = false, test_all_mas = false;
-	
-	cout << endl << "--------Nachalo resheniya---------" << endl;
-	
-	while(good == false)
-	{
-		double *prev_y_mas = nullptr;
-		prev_y_mas = new double [n_shag];
-		
-		for(int i = 0; i < n; i++) y_mas[i] = temp_y_mas[i];
-		du_4(x_first, x_last, h, y_mas, n, prev_y_mas);
-		
-		step++;
-//		cout << endl << "|-/--/--/--/-|Shag [ "<< step << " ] dvoinogo perecheta|--/--/--/--/-|" << endl;
-		
-		h /= 2;
-		n_shag = (x_last / h) - (x_first / h) + 1;
-		
-		double *next_y_mas = nullptr;
-		next_y_mas = new double [n_shag];
-		
-//		cout << endl << "h = " << h << endl;
-		
-		for(int i = 0; i < n; i++) y_mas[i] = temp_y_mas[i];
-		du_4(x_first, x_last, h, y_mas, n, next_y_mas);
-		
-//		cout << endl << "ddddddddddddddddddddddddddddddddddd" << endl;
-//		for(int i = 0; i < n_shag / 2; i++) cout << endl << prev_y_mas[i] << endl;
-//		cout << endl << "dddddddddddddddSSSSSSSSSSSSSSSSSdddddddddddddddddddd" << endl;
-//		for(int i = 0; i < n_shag; i++) cout << endl << next_y_mas[i] << endl;
-//		cout << endl << "ddddddddddddddddddddddddddddddddddd" << endl;
-		
-			for(int i = 1; i < (n_shag - 1) / 2 ; i++)
-			{
-//				cout << endl << " i = " << i << "  &&  i * 2 = " << i * 2 << "  n_shag = " << n_shag / 2 << endl;
-//				cout << endl << "y(h): " << prev_y_mas[i] << endl;
-//				cout << endl << "y(h/2): " << next_y_mas[i * 2] << endl;
-//				cout << endl << "Korektirovka: " << fabs(prev_y_mas[i] - next_y_mas[i * 2]) << " < " << 15 * eps << endl;
-				if(prev_y_mas[i] - next_y_mas[i * 2] < 15 * eps) test_all_mas = true; 
-				else
-				{
-//					cout << endl << "FALSE" << endl;
-					test_all_mas = false;
-					break;
-				}
-			}
+	double d1,d2;
+		d1=1/h1;
 
-		delete [] next_y_mas;
-		delete [] prev_y_mas;
+cout<<"\t"<<"Äëÿ h = "<<h1<<endl;
+cout<<endl;
+cout<<setw(15)<<" y "<<setw(15)<<" y'"<<setw(15)<<" x "<<endl;
+cout<<endl;
+	
+	while(k<d1)
+	{
+		k++;
+		x1[k-1]=x2[k-1];
+
+		y051[k]=y[k-1]+h1/2*yProiz[k-1];		
+		y052[k]=yProiz[k-1]+h1/2*f(x2[k-1],y[k-1],yProiz[k-1]);
+
 		
-		if(test_all_mas == true)
-		{
-			cout << endl << "----------------------------------";
-			cout << endl << "Otvet: " << endl;
-			for(int i = 0; i < n; i++) cout << endl << "	" << y_mas[i] << endl;
-			cout << endl << "Kolvo shagod: " << step << endl;
-			cout << endl << "h = : " << h;
-			good = true;
-		}
+		x1[k]=x1[k-1]+h1/2;
+		
+		x2[k]=x2[k-1]+h1; 
+		
+		y[k]=y[k-1]+h1*y052[k];
+		
+		yProiz[k]=yProiz[k-1]+h1*f(x1[k],y051[k],y052[k]);
+	//	cout<<setw(15)<<k<<setw(15)<<h1<<setw(15)<<y[k]<<setw(15)<<yProiz[k]<<setw(15)<<x2[k]<<setw(10)<<e<<endl;
+		cout<<setw(15)<<y[k]<<setw(15)<<yProiz[k]<<setw(15)<<x2[k]<<endl;
+		cout<<endl;
+		yOtv1[k-1]=y[k];
+		yProizOtv1[k-1]=yProiz[k];
+		
+		
 	}
 
-	cout << endl << "--------Konec resheniya---------" << endl;
+	cout<<endl;
+	
+	k=0;
+	t=50;
+	y[0]=1;
+	yProiz[0]=1;
+	x1[0]=0;
+	x2[0]=0;
+
+cout<<endl;
+cout<<"\t"<<"Äëÿ h = "<<h2<<endl;
+cout<<endl;
+
+cout<<setw(15)<<" y "<<setw(15)<<" y'"<<setw(15)<<" x "<<endl;
+
+d2=1/h2;
+
+	double hB1;
+	double hB2;
+
+int i=0;
+	//
+	while(k<d2)
+	{
+		k++;
+		x1[k-1]=x2[k-1];
+
+		y051[k]=y[k-1]+h2/2*yProiz[k-1];
+		y052[k]=yProiz[k-1]+h2/2*f(x2[k-1],y[k-1],yProiz[k-1]);
+		
+		x1[k]=x1[k-1]+h2/2;
+		x2[k]=x2[k-1]+h2; 
+		
+		y[k]=y[k-1]+h2*y052[k];
+		yProiz[k]=yProiz[k-1]+h2*f(x1[k],y051[k],y052[k]);
+	//	cout<<setw(15)<<k<<setw(15)<<h2<<setw(15)<<y[k]<<setw(15)<<yProiz[k]<<setw(15)<<x2[k]<<setw(10)<<e<<endl;
+	cout<<setw(15)<<y[k]<<setw(15)<<yProiz[k]<<setw(15)<<x2[k]<<setw(10)<<endl;
+		cout<<endl;
+
+	}
+	
+	
+	//
+	double e = 0.000001;	
+	//
+	double t1 = 1;
+	double t2 = 1;
+	
+	double Otv1[2];
+	double Otv2[2];
+	
+	h1=0.1;
+	h2=0.05;
+	cout<<setw(15)<<"h"<<setw(15)<<"x"<<setw(15)<<"y"<<setw(15)<<"y'"<<endl;
+	cout<<endl;
+	while (t1 >= e && t2 >= e)
+	{
+		
+	double d;
+	d=1/h1;
+	double d1;
+	d1=1/h2;
+	//
+	double x = 0;
+	double x1 = 0;
+	//
+	double y[2];
+	double y1[2];
+	double yProiz[2];
+	double yProiz1[2];
+	double y051[2];
+	double y0511[2];
+	
+	//
+	double y0 = 1;
+	double y01 = 1;
+	double yProiz0 = 1;
+	double yProiz01 = 1;
+	//
+	yProiz[0] = y0;
+	yProiz[1] = yProiz0;
+
+	for (int i = 0; i < d; i++)
+	{
+
+		
+		y051[0] = yProiz[0] + (h1 / 2) * yProiz[1];
+		y051[1] = yProiz[1] + (h1 / 2) * f(x, yProiz[0], yProiz[1]);
+		y[0] = yProiz[0] + h1 * y051[1];
+		y[1] = yProiz[1] + h1 * f(x + h1 / 2, y051[0], y051[1]);
+		
+		x = x + h1;
+		yProiz[0] = y[0];
+		yProiz[1] = y[1];
+		hB1=h1;
+		
+	}
+		cout<<setw(15)<<h1<<setw(15)<<x<<setw(15)<< y[0] << setw(15)<<y[1]<<endl;
+		Otv1[0]=y[0];
+		Otv1[1]=y[1];
+			
+	yProiz1[0] = y0;
+	yProiz1[1] = yProiz0;
+		for (int i = 0; i < d1; i++)
+	{
+
+	
+		y0511[0] = yProiz1[0] + (h2 / 2) * yProiz1[1];
+		y0511[1] = yProiz1[1] + (h2 / 2) * f(x1, yProiz1[0], yProiz1[1]);
+		y1[0] = yProiz1[0] + h2 * y0511[1];
+		y1[1] = yProiz1[1] + h2 * f(x1 + h2 / 2, y0511[0], y0511[1]);
+		
+		x1 = x1 + h2;
+		yProiz1[0] = y1[0];
+		yProiz1[1] = y1[1];
+		hB2=h2;
+	//	cout<<setw(15)<<h2<<setw(15)<<x<<setw(15)<< y[0] << setw(15)<<y[1]<<endl<<endl;
+	}
+		cout<<setw(15)<<h2<<setw(15)<<x<<setw(15)<< y1[0] << setw(15)<<y1[1]<<endl;
+		Otv2[0]=y1[0];
+		Otv2[1]=y1[1];
+			
+		t1 = abs(Otv1[0] - Otv2[0]);
+		t2 = abs(Otv1[1] - Otv2[1]);
+		h2 /= 2;
+		h1 /= 2;
+		cout<<endl;
+
+	}
+	
 	
 	return 0;
 }
--- kurs2/VIochMath/laby_rubanchiku/kurskurs/kurs_du.cpp
+++ kurs2/VIochMath/laby_rubanchiku/kurskurs/kurs_du.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const double eps = 0.00001, e = 2.71828182846;
+const double eps = 0.000001;
 
 int opredelitel()
 {
@@ -15,7 +15,7 @@ int opredelitel()
 
 double formula(double x, double *y_mas)
 {
-	return (pow(e, x) + y_mas[0] + y_mas[1]) / 3;
+	return (exp(x) + y_mas[0] + y_mas[1]) / 3;
 //	return x * y_mas[1] + y_mas[0];
 // 	return x + y_mas[0] + x * y_mas[1] - y_mas[2];
 }
@@ -38,7 +38,6 @@ void du_4(double x_first, double x_last, double h, double *y_mas, int n, double 
 	{
 		save_y_mas[i] = y_mas[i];
 //		cout << endl << save_y_mas[i] << endl;
-		y_mas[i] = 0;
 	}
 //	cout << endl << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO EVROPA OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << endl;
 //	for(int i = 0; i < n; i++) cout << endl << "y_mas = " << y_mas[i] << endl;
@@ -50,49 +49,39 @@ void du_4(double x_first, double x_last, double h, double *y_mas, int n, double 
 //		cout << endl << "x = " << x << endl;
 		
 //		cout << endl << "--------\\" << endl;
-		for(int k = 1; k < 5; k++)
+		for(int q = n - 1, z = 0; q > 0; q--, z++)
 		{
-			if(k == 1)
-			{
-				for(int q = n - 1, z = 0; q > 0; q--, z++)
-				{
-					k_mas[0][z] = save_y_mas[q];
+			k_mas[0][z] = save_y_mas[q];
 //					cout << endl << "Neochen test y_mas = " << save_y_mas[q] << "  i konechno je q = " << q;
 //					cout << endl << "Neochen test k_mas = " << k_mas[0][z] << "  i konechno je z = " << z;
-				}
-				k_mas[0][n - 1] = formula(x, save_y_mas);
-//				for(int i = 0; i < n; i++) cout << endl << "	k1_mas = " << k_mas[0][i] << endl;
-			}
-			else if(k == 2)
-			{
-				for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[0][i];
-				for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[1][z] = y_mas[q];
-				k_mas[1][n - 1] = formula(x + h / 2, y_mas);
-				for (int i = 0; i < n; i++) y_mas[i] = 0;
-//				for(int i = 0; i < n; i++) cout << endl << "	k2_mas = " << k_mas[1][i] << endl;
-			}
-			else if(k == 3)
-			{
-				for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[1][i];
-				for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[2][z] = y_mas[q];;
-				k_mas[2][n - 1] = formula(x + h /2, y_mas);
-				for (int i = 0; i < n; i++) y_mas[i] = 0;
-//				for(int i = 0; i < n; i++) cout << endl << "	k3_mas = " << k_mas[2][i] << endl;
-			}
-			else if(k == 4)
-			{
-				for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[2][i];
-				for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[3][z] = y_mas[q];;
-				k_mas[3][n - 1] = formula(x + h, y_mas);
-				for (int i = 0; i < n; i++) y_mas[i] = 0;
-//				for(int i = 0; i < n; i++) cout << endl << "	k4_mas = " << k_mas[3][i] << endl;
-			}
 		}
+		k_mas[0][n - 1] = formula(x, save_y_mas);
+//		k_mas[0][n - 1] = round((formula(x, save_y_mas) / eps)) * eps;
+//		for(int i = 0; i < n; i++) cout << endl << "	k1_mas = " << k_mas[0][i] << endl;
+	
+		for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[0][i];
+		for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[1][z] = y_mas[q];
+		k_mas[1][n - 1] = formula(x + h / 2, y_mas);
+//		k_mas[1][n - 1] = round((formula(x + h / 2, y_mas)) / eps) * eps;
+//		for(int i = 0; i < n; i++) cout << endl << "	k2_mas = " << k_mas[1][i] << endl;
+	
+		for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + (h / 2) * k_mas[1][i];
+		for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[2][z] = y_mas[q];
+		k_mas[2][n - 1] = formula(x + h / 2, y_mas);
+//		k_mas[2][n - 1] = round((formula(x + h /2, y_mas)) / eps) * eps;
+//		for(int i = 0; i < n; i++) cout << endl << "	k3_mas = " << k_mas[2][i] << endl;
+
+		for(int i = 0; i < n; i++) y_mas[i] = save_y_mas[i] + h * k_mas[2][i];
+		for(int q = n - 1, z = 0; q > 0; q--, z++) k_mas[3][z] = y_mas[q];
+		k_mas[3][n - 1] = formula(x + h, y_mas);
+//		k_mas[3][n - 1] = round((formula(x + h, y_mas)) / eps) * eps;
+//		for(int i = 0; i < n; i++) cout << endl << "	k4_mas = " << k_mas[3][i] << endl;
 //		cout << endl << "--------/" << endl;
 		
 		for(int i = 0; i < n; i++)
 		{
-			y_mas[i] =  save_y_mas[i] + h / 6 * (k_mas[0][i] + 2 * k_mas[1][i] + 2 * k_mas[2][i] + k_mas[3][i]);
+			y_mas[i] =  save_y_mas[i] + (h / 6) * (k_mas[0][i] + 2 * k_mas[1][i] + 2 * k_mas[2][i] + k_mas[3][i]);
+//			y_mas[i] =  round((save_y_mas[i] + h / 6 * (k_mas[0][i] + 2 * k_mas[1][i] + 2 * k_mas[2][i] + k_mas[3][i])) / eps) * eps;
 			save_y_mas[i] = y_mas[i];
 		}
 		
@@ -110,7 +99,7 @@ void du_4(double x_first, double x_last, double h, double *y_mas, int n, double 
 
 main()
 {
-	double x_first, x_last, h, I1, I2;
+	double x_first, x_last, h;
 	
 	cout << endl << "1) x_first\n2) x_last\n3) Shag" << endl;
 	cin >> x_first >> x_last >> h;
@@ -160,24 +149,28 @@ main()
 		for(int i = 0; i < n; i++) y_mas[i] = temp_y_mas[i];
 		du_4(x_first, x_last, h, y_mas, n, next_y_mas);
 		
-//		cout << endl << "ddddddddddddddddddddddddddddddddddd" << endl;
-//		for(int i = 0; i < n_shag / 2; i++) cout << endl << prev_y_mas[i] << endl;
-//		cout << endl << "dddddddddddddddSSSSSSSSSSSSSSSSSdddddddddddddddddddd" << endl;
-//		for(int i = 0; i < n_shag; i++) cout << endl << next_y_mas[i] << endl;
-//		cout << endl << "ddddddddddddddddddddddddddddddddddd" << endl;
+		cout << endl << "ddddddddddddddddddddddddddddddddddd" << endl;
+		for(int i = 0; i < n_shag / 2; i++) cout << endl << prev_y_mas[i] << endl;
+		cout << endl << "dddddddddddddddSSSSSSSSSSSSSSSSSdddddddddddddddddddd" << endl;
+		for(int i = 0; i < n_shag; i++) cout << endl << next_y_mas[i] << endl;
+		cout << endl << "ddddddddddddddddddddddddddddddddddd" << endl;
 		
 			for(int i = 1; i < (n_shag - 1) / 2 ; i++)
 			{
-//				cout << endl << " i = " << i << "  &&  i * 2 = " << i * 2 << "  n_shag = " << n_shag / 2 << endl;
-//				cout << endl << "y(h): " << prev_y_mas[i] << endl;
-//				cout << endl << "y(h/2): " << next_y_mas[i * 2] << endl;
-//				cout << endl << "Korektirovka: " << fabs(prev_y_mas[i] - next_y_mas[i * 2]) << " < " << 15 * eps << endl;
-				if(prev_y_mas[i] - next_y_mas[i * 2] < 15 * eps) test_all_mas = true; 
+				cout << endl << " i = " << i << "  &&  i * 2 = " << i * 2 + 1 << "  n_shag = " << n_shag / 2 << " h =  " << h << endl;
+				cout << endl << "y(h): " << prev_y_mas[i] << endl;
+				cout << endl << "y(h/2): " << next_y_mas[i * 2 + 1] << endl;
+				cout << endl << "Korektirovka: " << fabs(prev_y_mas[i] - next_y_mas[i * 2 + 1]) << " < " << 15 * eps << endl;
+				if(fabs((prev_y_mas[i] - next_y_mas[i * 2 + 1])) < (15 * eps)) test_all_mas = true; 
 				else
 				{
-//					cout << endl << "FALSE" << endl;
+					cout << endl << "FALSE" << endl;
+					cout << endl << " i = " << i << "  &&  i * 2 = " << i * 2 << "  n_shag = " << n_shag / 2 << " h =  " << h << endl;
+					cout << endl << "y(h): " << prev_y_mas[i] << endl;
+					cout << endl << "y(h/2): " << next_y_mas[i * 2 + 1] << endl;
+					cout << endl << "Korektirovka: " << fabs(prev_y_mas[i] - next_y_mas[i * 2 + 1]) << " < " << 15 * eps << endl;
 					test_all_mas = false;
-					break;
+					i = n_shag;
 				}
 			}
 
