1362e123de62d69f23414b7bce26fbfa19b7fa77
Snuper <47518901+Snuper@users.noreply.github.com>
2020-09-12 19:03:55 +0700

    1 лаба по Архитектуре

--- kurs3/avs/lab1/script.sh
+++ kurs3/avs/lab1/script.sh
@@ -1,46 +1,39 @@
 #!/bin/bash
-date
-whoami
-hostname
-
+echo " Дата: $(date | awk '{print $2, $3, $4}')"
+echo "Имя учетной записи: $(whoami)"
+echo "Доменное имя учетной записи: $(hostname)"
 echo "Процессор:"
-echo -e -n "\t• Модель — "
-lscpu|grep Имя  | awk '{print $3, $4, $5}'
-echo -e -n "\t• Архитектура — "
-lscpu|grep Архитектура| awk '{print $NF}'
-echo -e -n "\t• Тактовая частота — "
-lscpu|grep Имя| awk '{print $NF}' 
-echo -e -n "\t• Количество ядер — "
-lscpu|grep Ядер| awk '{print $NF}' 
-echo -e -n "\t• Количество потоков на одно ядро — "
-lscpu|grep Thread| awk '{print $NF}' 
-
-echo  "Оперативная память:"
-echo -e -n "\t• Всего — "
-free -h | grep Память | awk '{print $2}'
-echo -e -n "\t• Доступно — "
-free | grep Память | awk '{print $7}'
-
+echo " • Модель$(cat /proc/cpuinfo | grep "model name" | uniq | awk '{print $3, $4, $5, $6, $7, $8, $9}')"
+echo " • Архитектура: $(uname -m)"
+echo " • Частота: $(cat /proc/cpuinfo | grep "cpu MHz" | uniq | awk '{print $4}')"
+echo " • Кол-во ядер: $(cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $4}')"
+echo " • Кол-во потоков на одно ядро: $(cat /proc/cpuinfo | grep "siblings" | uniq | awk '{print $3}')"
+echo "Оперативная память:"
+echo " • Всего: $(cat /proc/meminfo | grep "MemTotal" | awk '{print $2, $3}')"
+echo " • Доступно: $(cat /proc/meminfo | grep "MemFree" | awk '{print $2, $3}')"
 echo  "Жесткий диск:"
 echo -e -n "\t• Всего — "
-df | grep "/$" | awk '{print $3}'
+df -h | grep "/$" | awk '{print $2}'
 echo -e -n "\t• Доступно — "
-df | grep "/$" | awk '{print $4}'
+df -h | grep "/$" | awk '{print $3}'
 echo -e -n "\t• Смонтировано в корневую директорию — "
-df | grep "/$" | awk '{print $1}'
+df -h | grep "/$" | awk '{print $1}'
 echo -e -n "\t• SWAP всего — "
-free | grep Подкачка | awk '{print $2}'
+free -h | grep Подкачка | awk '{print $2}'
 echo -e -n "\t• SWAP доступно — "
-free | grep Подкачка | awk '{print $4}'
-
+free -h | grep Подкачка | awk '{print $4}'
 echo  "Сетевые интерфейсы:"
-echo -e -n "\t• Количество сетевых интерфейсов — "
+echo -e -n "\t• Количество сетевых интерфейсов: "
 ls /sys/class/net | wc -l
 echo
 echo -e "Имя сетевого интерфейса\t\tMAC адрес\t\tIP адрес\t\tСкорость соединения"
 for iface in /sys/class/net/*; do
+	if [[ "lo" = "${iface##*/}" ]]; then
+	echo "lo -"
+else
 	echo -ne "${iface##*/}\t\t\t\t";
 	tr "\n" "\t" < "$iface/address";
-	printf "%-24s" " $(ip -4 -o a | grep " ${iface##*/} " | tr -s ' ' |  cut -d ' ' -f4 | tr -d '\n')"; 
-	ip -4 -0 a | grep " ${iface##*/}: " | tr -s ' ' | cut -d' ' -f13
-done  
+	printf "%-24s" " $(ip -4 -o a | grep " ${iface##*/} " | tr -s ' ' |  cut -d ' ' -f4 | tr -d '\n')";
+	echo " $(ip -4 -0 a | grep " ${iface##*/}: " | tr -s ' ' | cut -d' ' -f13) Mibit"
+	fi
+done
